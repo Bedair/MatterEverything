a326977f81bf29b1844c6908b59f27b2f14542fa
Bedair <m.bedair94@gmail.com>
2024-10-09 19:27:39 +0300

    Add implementation for Relay control

--- Code/EverythingMatter/EverythingMatter.ino
+++ Code/EverythingMatter/EverythingMatter.ino
@@ -4,14 +4,28 @@
 
 #include <LiquidCrystal.h>
 
-// with the arduino pin number it is connected to
+/* LCD Pins*/
 const int rs = 12, en = 11, d4 = 10, d5 = 9, d6 = 8, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+/* Relay */
+#define RELAY_PIN                 (A7)
+void inline Relay_Init(void) {pinMode(RELAY_PIN, OUTPUT);}
+void inline Relay_Enable_Output(void) {digitalWrite(RELAY_PIN, LOW);}
+void inline Relay_Disable_Output(void) {digitalWrite(RELAY_PIN, HIGH);}
+
 
 void setup() {
   /* Initialize the LCD */
   lcd.begin(16, 2);
   lcd.print("**** Matter ****");
+
+  /* Initialize Relay */
+  Relay_Init();
+  Relay_Disable_Output();
+  delay(1000);
+
 }
 
 void loop() {
@@ -21,6 +35,8 @@ void loop() {
   lcd.print("Current: ");
   lcd.print(1.525412, 3);
   lcd.print(" A");
+
+  Relay_Enable_Output();
 
 }
 
